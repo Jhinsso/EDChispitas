9317dc4729360b01fb1ccb78bc53846275b1e18d
Jorge Barrio <115726013+Jhinsso@users.noreply.github.com>
2022-12-02 00:16:56 +0100

    f5

--- EDEmpresaChispitas/ElectricidadChispas/src/EjChispas/ventas.java
+++ EDEmpresaChispitas/ElectricidadChispas/src/EjChispas/ventas.java
@@ -15,4 +15,28 @@ public class ventas{
     private String email;
 
     private int telefono;
+
+    public void print(Facturas invoice){}
+    public String getNombre(){return nombre;}
+    public String getApellidos(){return apellidos;}
+    public String getNif(){return nif;}
+    public String getPoblacion(){return poblacion;}
+    public String getProvincia(){return provincia;}
+    public String getEmail(){return email;}
+
+    public int getCodigoCliente(){return codigoCliente;}
+    public int getCodigoPostal(){return codigoPostal;}
+    public int getTelefono(){return telefono;}
+
+
+    public void setNombre(String nombre){this.nombre=nombre;}
+    public void setApellidos(String apellidos){this.apellidos=apellidos;}
+    public void setNif(String nif){this.nif=nif;}
+    public void setPoblacion(String poblacion){this.poblacion=poblacion;}
+    public void setProvincia(String provincia){this.provincia=provincia;}
+    public void setEmail(String email){this.email=email;}
+
+    public void setCodigoCliente(int codigoCliente){this.codigoCliente=codigoCliente;}
+    public void setCodigoPostal(int codigoPostal){this.codigoPostal=codigoPostal;}
+    public void setTelefono(int telefono){this.telefono=telefono;}
 }
